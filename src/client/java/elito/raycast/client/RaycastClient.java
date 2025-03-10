aa5735c445018e03e2d7c950dd8acabe02a41f07
Edward <evelasquez0@ucol.mx>
2025-03-10 16:37:40 -0600

    Initial commit

---
+++ src/client/java/elito/raycast/client/RaycastClient.java
@@ -0,0 +1,10 @@
+package elito.raycast.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RaycastClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/elito/raycast/Raycast.java
@@ -0,0 +1,10 @@
+package elito.raycast;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Raycast implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
