bca6f7ab6add1dde8abae19d32164cdb0634ce9f
ElitoSear <elitogamer100@gmail.com>
2025-03-21 00:35:04 -0600

    - Custom branch for personal propourses.

--- src/main/java/elito/raycast/Raycast.java
+++ src/main/java/elito/raycast/Raycast.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 
 public class Raycast implements ModInitializer {
 
-    public static final String MOD_ID = "raycast";
+    public static final String MOD_ID = "raycast_surfaced";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
