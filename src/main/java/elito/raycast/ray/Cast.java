c8e8772bd49cc459caf6d87cf7f31245e728d1a4
ElitoSear <elitogamer100@gmail.com>
2025-04-01 01:21:00 -0600

    Bumped version to 1.21.5.

--- src/main/java/elito/raycast/ray/Cast.java
+++ src/main/java/elito/raycast/ray/Cast.java
@@ -24,12 +24,12 @@ public class Cast {
 
     public static Vec2f getRotation(Direction direction) {
 
-        return switch (direction.getName()) {
-            case "south" -> new Vec2f(0, 0);
-            case "east" -> new Vec2f(-90, 0);
-            case "west" -> new Vec2f(90, 0);
-            case "up" -> new Vec2f(0, -90);
-            case "down" -> new Vec2f(0, 90);
+        return switch (direction) {
+            case SOUTH -> new Vec2f(0, 0);
+            case EAST -> new Vec2f(-90, 0);
+            case WEST -> new Vec2f(90, 0);
+            case UP -> new Vec2f(0, -90);
+            case DOWN -> new Vec2f(0, 90);
             default -> new Vec2f(-180, 0);
         };
     }
